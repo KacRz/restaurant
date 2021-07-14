ece3be3027ea3e225cf6e5a4db21aa844446df3c
szczerrzuja <57622414+szczerrzuja@users.noreply.github.com>
2021-07-14 08:58:38 +0200

    Baner component prop done

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,53 @@
 <template>
+<<<<<<< Updated upstream
   <div id="app">
     <Sidebar />
     <div :style=" { 'margin-left': sidebarWidth } ">
       
       <router-view/>
     </div>
+=======
+  <Sidebar />
+<div class="MarginToSidebar" :style="{ 'margin-left': sidebarWidth }">
+<Header style="width: 100%" />
+<div style="margin-left: 15px">
+  Chleb – pieczywo otrzymywane z wypieku ciasta, będącego jednorodnym połączeniem mąki zbożowej różnego gatunku i wody, poddanego najczęściej, 
+  ale nie zawsze, fermentacji alkoholowej (z wykorzystaniem drożdży), wyrobionego w bochny różnego kształtu i wielkości. Oprócz chleba pulchnego, 
+  wyrabianego z ciasta drożdżowego lub ciasta z dodatkiem proszku do pieczenia (kwaśny węglan sodu), znane są też odmiany chleba opartego na niespulchnianym cieście kruchym.
+
+Chleb znany w starożytności nie przypominał współczesnego pulchnego wypieku, raczej zbliżony był do podpłomyka, znanej obecnie macy 
+lub cienkiego podkładu pod pizzę. Taki chleb łatwiej było łamać niż kroić i zapewne stąd wziął się powszechnie znany w wielu kulturach 
+zwyczaj obrzędowego łamania chlebem. Zwyczaj łamania, a nie krojenia chleba, utrzymał się zresztą do dzisiaj w kulturze śródziemnomorskiej (m.in. we Francji).
+
+Słowo chleb jest prawdopodobnie pochodzenia praindoeuropejskiego, występuje w językach słowiańskich, pragermańskich i sanskrycie[1].
+
+Wedle innej wersji chleb jest zapożyczeniem z języka starogermańskiego – hlaiba, starogockie hlaifs, staroislandzkie hleifr[2].
+
+Według szacunków Instytutu Polskie Pieczywo w 2015 r. statystyczny Polak zjadł 45,3 kg pieczywa, czyli o 2,5 kg mniej niż rok 
+wcześniej[3] (2014 – 47,8 kg, 2010 – 56,7 kg, 2009 – 59 kg, 2008 – 61 kg). Tymczasem jeszcze w 1981 przeciętny mieszkaniec tego 
+kraju spożywał ok. 100 kg chleba, a w początkach XXI wieku – około 80 kg[4]. 
+</div>
+</div>
+
+  <div :style=" { 'margin-left': sidebarWidth } ">
+    <router-view/>
+>>>>>>> Stashed changes
   </div>
+
 </template>
 
 <script>
+
 import Sidebar from '@/components/sidebar/Sidebar.vue'
 import { sidebarWidth } from '@/components/sidebar/state'
+import Header from './components/Header.vue'
 
 export default {
   components: {
-    Sidebar
+    Sidebar,
+    Header,
   },
-  setup() {
+    setup() {
     return { sidebarWidth }
   }
 }
@@ -24,11 +55,20 @@ export default {
 
 
 <style>
+<<<<<<< Updated upstream
 body {
   width: 100%;
   height: 100%;
   margin: 0;
   padding: 0;
+=======
+body, html
+{
+
+  width:100%;
+  height: 100%;
+  margin: 0;
+>>>>>>> Stashed changes
 }
 
 #app {
@@ -55,5 +95,11 @@ body {
 }
 #nav a.router-link-exact-active {
   color: #42b983;
+  
+}
+
+.MarginToSidebar
+{
+  height: 100%;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    
+    <a  href="/" style="height: 200px; width: 200px;"><img src="@/assets/Logo.jpg" alt="" > </a>
+</div>
+</template>
+<script>
+
+export default {
+name: "Header"
+}
+</script>
+<style scoped>
+img
+{
+    width: 100%;
+    height: 150px;
+}
+div{
+    margin:0px;
+}
+</style>
